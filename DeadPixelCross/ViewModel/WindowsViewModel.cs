36060af0e5b7d907af7fca31e13bffefff58de97
Alexander Pototsky <silencemyalise@gmail.com>
2022-12-05 02:40:22 +0600

    Add easy color test

--- DeadPixelCross/ViewModel/WindowsViewModel.cs
+++ DeadPixelCross/ViewModel/WindowsViewModel.cs
@@ -11,10 +11,26 @@ namespace DeadPixelCross.ViewModel
 {
     partial class WindowsViewModel : GeneralViewModel
     {
+        private int countColor = 0;
+        private List<Color> _colorsList = new() { Colors.White, Colors.Red, Colors.Green, Colors.Blue, Colors.Black };
+
         public WindowsViewModel()
         {
             _platformName = "Windows";
             _versionProgramm = "Version: 0.0.1 - test";
+        }
+
+
+        [ObservableProperty]
+        public Color colorWindow = Microsoft.Maui.Graphics.Colors.White;
+
+        [RelayCommand]
+        public void ChangeColorInBtn()
+        {
+            if(countColor == 4 ) { countColor = -1;}
+            countColor++;
+            colorWindow = _colorsList[countColor];
+            OnPropertyChanged(nameof(ColorWindow));
         }
     }
 }
